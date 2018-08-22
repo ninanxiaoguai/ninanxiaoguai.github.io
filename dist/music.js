33300dd9846b939daf13b2da421fc292a4367b73
VON297 <1370076020@qq.com>
2018-08-22 16:43:54 +0800

    Site updated: 2018-08-22 16:43:50

--- dist/music.js
+++ dist/music.js
@@ -7,19 +7,19 @@ const ap = new APlayer({
         name: "Dream It Possible",
         artist: 'Delacey',
         url: 'http://www.ytmp3.cn/down/47868.mp3',
-        cover: 'source/images/music.png',
+        cover: 'https://cdn-img.easyicon.net/png/5473/547376.gif',
       },
       {
         name: 'いとしすぎて',
         artist: 'KG',
         url: 'http://www.ytmp3.cn/down/35726.mp3',
-        cover: 'source/images/music.png',
+        cover: 'https://cdn-img.easyicon.net/png/5473/547376.gif',
       },
       {
         name: '茜さす',
         artist: 'Aimer',
         url: 'http://www.ytmp3.cn/down/44578.mp3',
-        cover: 'source/images/music.png',
+        cover: 'https://cdn-img.easyicon.net/png/5473/547376.gif',
       }
     ]
 });
